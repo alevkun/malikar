3b98206e43a795b5040d6e5f3f08b3bd990f9342
Alexander Levkun <alexanderlevkun@gmail.com>
2015-05-29 16:29:58 +0300

    update description

---
+++ R/malikar.R
@@ -0,0 +1,13 @@
+#' square number
+#' 
+#' function that squares number
+#' 
+#' This important function may be used to square any number
+#' @param x the number that will be squared
+#' @param n the power (default is 2)
+#' @return x power n
+#' @export
+#' @examples
+#' malika(5)
+#' malika(5, n = 6)
+malika <- function(x, n = 2) x^n
